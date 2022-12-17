26e381c80a01d13458eda977039ae3bd6ec1ce36
Krzysztof Pagacz <krzysiekpagacz@gmail.com>
2022-12-17 11:10:09 +0100

    unit tests and mockito added to pom.xml; TestConstants class added

---
+++ starter_code/src/test/java/com/example/demo/TestConstants.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.math.BigDecimal;
+
+public class TestConstants {
+	
+	public final static String ANY_USERNAME = "fakeUser";
+	public final static String ANY_PASSWORD = "fakePassword";
+	public final static String ANY_SHORT_PASSWORD = "fake";
+	public final static String ANY_HASHED_PASSWORD = "fakeHashedPassword";
+	public final static long ANY_USER_ID = 999999L;
+	public static final long ANY_ITEM_ID = 1L;
+	public static final long ANY_CART_ID = 1L;
+	public static final long ANY_USER_ORDER_ID = 1L;
+	public static final String ANY_ITEM_NAME = "T-Shirt";
+	public static final BigDecimal ANY_ITEM_PRICE = BigDecimal.valueOf(9.99);
+	public static final int ANY_ITEM_QUANTITY = 10;
+	public static final String ANY_ITEM_DESCRIPTION = "Blue cotton t-shirt for women";
+
+}
